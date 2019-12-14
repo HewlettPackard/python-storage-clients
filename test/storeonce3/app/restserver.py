33008c320dd4377358fe02545db54b924842eced
Ivan Smirnov <ivan.smirnov@hpe.com>
2019-12-15 01:16:59 +0300

    Autotests for StoreOnceG3 branch init

---
+++ test/storeonce3/app/restserver.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#   (C) Copyright 2017-2019 Hewlett Packard Enterprise Development LP
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+"""
+.. module:: test.storeonce3
+    :synopsis: Flask server for testing hpestorapi.StoreOnceG3
+
+.. moduleauthor:: Ivan Smirnov <ivan.smirnov@hpe.com>, HPE Pointnext DACH & Russia
+"""
+
+
+from flask import Flask
+from flask_restful import Api
+
+
+class ApiInstance:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app)
+
+    def run(self):
+        self.app.run(debug=False, host='0.0.0.0', port=443)
+
+
+if __name__ == "__main__":
+    server = ApiInstance()
+    server.run()
